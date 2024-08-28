4b12a47f32fe10df14308ebf524becc1e41bc34f
Esteban Roman <esteban.roman@uao.edu.co>
2024-08-28 02:54:34 -0500

    Inicializando el repositorio con archivos

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// Aquí puedes añadir funcionalidades interactivas en el futuro
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Document loaded. Ready for interaction.");
+});
